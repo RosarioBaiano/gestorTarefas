7c763034f48fcfff16def95bc61e355e7e7dd1b1
Rosário Baiano <aleixobaiano@icloud.com>
2024-12-19 00:35:24 +0000

    criando componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'tasklist', title: 'TaskList', component: TaskListComponent},
+    {path: 'taskdetaisl', title: 'TaskDetails', component: TaskDetailsComponent},
+    {path: '', redirectTo: '/ tasklist', pathMatch: 'full'},
+];
