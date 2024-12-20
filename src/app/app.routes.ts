473d1f830d38d459a9ef118c6e2d1ac7af0a9c66
Rosário Baiano <aleixobaiano@icloud.com>
2024-12-20 00:36:03 +0000

    html dos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailsComponent } from './task-details/task-details.component';
+import { TaskFilterComponent } from './task-filter/task-filter.component';
 
 export const routes: Routes = [
     {path: 'tasklist', title: 'TaskList', component: TaskListComponent},
-    {path: 'taskdetaisl', title: 'TaskDetails', component: TaskDetailsComponent},
-    {path: '', redirectTo: '/ tasklist', pathMatch: 'full'},
+    {path: 'taskdetails', title: 'TaskDetails', component: TaskDetailsComponent},
+    {path: 'taskfilter', title: 'TaskFilter', component: TaskFilterComponent},
+    {path: '', redirectTo: '/tasklist', pathMatch: 'full'},
 ];
